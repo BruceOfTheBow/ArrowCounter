c1cfa72e37b36e6195e8f1cc3752def5d15d1db5
BruceOfTheBow <bruceofthebow@gmail.com>
2022-08-02 22:05:00 -0700

    Added saving of arrow counts to text file in configuration directory. Arrow counts load from file on mod initialization. Will need to be tracked by player and read/saved on player login/logout.

--- ArrowCounter.cs
+++ ArrowCounter.cs
@@ -1,7 +1,8 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Linq;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
@@ -17,38 +18,49 @@ namespace ArrowCounter
         public const string PluginName = "ArrowCounter";
         public const string PluginVersion = "1.0.0";
 
+        private const string countsFileName = "arrowCounts";
+        private static ConfigFile configFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "ArrowCounter.cfg"), true);
+
         static ManualLogSource _logger;
         Harmony _harmony;
         static ConfigEntry<bool> isModEnabled;
+
+        public static bool ArrowCounterUIEnabled = true;
 
         public void Awake()
         {
             isModEnabled = Config.Bind<bool>("_Global", "isModEnabled", true, "Enable or disable this mod.");
             _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGuid);
             _logger = Logger;
+            Attack_UseAmmo_Patch.initializeArrowCounts();
         }
 
         public void OnDestroy()
         {
+            // this should really go on logout
+            Attack_UseAmmo_Patch.saveCounts();
             _harmony?.UnpatchSelf();
         }
 
 
         [HarmonyPatch(typeof(Attack))]
-        public static class Attack_UseAmmo_Patch
-        {
+        public static class Attack_UseAmmo_Patch {
             private static bool _debug = true;
-            private static int woodArrowCount = 0;
-            private static int fireArrowCount = 0;
-            private static int flintArrowCount = 0;
-            private static int bronzeArrowCount = 0;
-            private static int ironArrowCount = 0;
-            private static int silverArrowCount = 0;
-            private static int obsidianArrowCount = 0;
-            private static int poisonArrowCount = 0;
-            private static int frostArrowCount = 0;
-            private static int needleArrowCount = 0;
-            
+
+            private static List<string> arrowNames = new List<string> {
+                    "$item_arrow_wood",
+                    "$item_arrow_fire",
+                    "$item_arrow_flint",
+                    "$item_arrow_bronze",
+                    "$item_arrow_iron",
+                    "$item_arrow_silver",
+                    "$item_arrow_obsidian",
+                    "$item_arrow_poison",
+                    "$item_arrow_frost",
+                    "$item_arrow_needle"
+                };
+            private static Dictionary<string, int> arrowCounts = initializeArrowCounts();            
+
             [HarmonyPostfix]
             [HarmonyPatch(nameof(Attack.UseAmmo))]
             
@@ -63,56 +75,8 @@ namespace ArrowCounter
             {
                 if(ammoType.Equals("$ammo_arrows"))
                 {
-                    if (ammoName.Equals("$item_arrow_wood"))
-                    {
-                        woodArrowCount++;
-                        log(woodArrowCount.ToString() + " wood arrows fired this session.");
-                    }
-                    else if(ammoName.Equals("$item_arrow_fire"))
-                    {
-                        fireArrowCount++;
-                        log(fireArrowCount.ToString() + " fire arrows fired this session.");
-                    } 
-                    else if(ammoName.Equals("$item_arrow_flint"))
-                    {
-                        flintArrowCount++;
-                        log(flintArrowCount.ToString() + " flint arrows fired this session.");
-                    }
-                    else if (ammoName.Equals("$item_arrow_bronze"))
-                    {
-                        bronzeArrowCount++;
-                        log(bronzeArrowCount.ToString() + " bronze arrows fired this session.");
-                    }
-                    else if (ammoName.Equals("$item_arrow_iron"))
-                    {
-                        ironArrowCount++;
-                        log(ironArrowCount.ToString() + " iron arrows fired this session.");
-                    }
-                    else if (ammoName.Equals("$item_arrow_silver"))
-                    {
-                        silverArrowCount++;
-                        log(silverArrowCount.ToString() + " silver arrows fired this session.");
-                    }
-                    else if (ammoName.Equals("$item_arrow_obsidian"))
-                    {
-                        obsidianArrowCount++;
-                        log(obsidianArrowCount.ToString() + " obsidian arrows fired this session.");
-                    }
-                    else if (ammoName.Equals("$item_arrow_poison"))
-                    {
-                        poisonArrowCount++;
-                        log(poisonArrowCount.ToString() + " poison arrows fired this session.");
-                    }
-                    else if (ammoName.Equals("$item_arrow_frost"))
-                    {
-                        frostArrowCount++;
-                        log(frostArrowCount.ToString() + " frost arrows fired this session.");
-                    }
-                    else if (ammoName.Equals("$item_arrow_needle"))
-                    {
-                        needleArrowCount++;
-                        log(needleArrowCount.ToString() + " needle arrows fired this session.");
-                    }
+                    arrowCounts[ammoName]++;
+                    log(arrowCounts[ammoName].ToString() + " " + ammoName.Split('_').Last() + " arrows fired in total.");
                     return true;
                 }
                 return false;
@@ -124,6 +88,39 @@ namespace ArrowCounter
                 {
                     _logger.LogInfo(message);
                 }
+            }
+            
+            public static void GetCounts() {
+
+            }
+
+            public static void saveCounts() {
+                log("Arrow count saving to file: " + getCountsFilePath());
+                using (StreamWriter file = File.CreateText(getCountsFilePath())) {
+                    foreach (KeyValuePair<string, int> entry in arrowCounts) {
+                        file.WriteLine(entry.Key + "," + entry.Value.ToString());
+                    }
+                }
+            }
+            public static Dictionary<string, int> initializeArrowCounts() {
+                Dictionary<string, int> arrowCounts = new Dictionary<string, int>();
+                if (File.Exists(getCountsFilePath())) {
+                    log("Reading data from " + getCountsFilePath());
+                    string[] lines = File.ReadAllLines(getCountsFilePath());
+                    foreach(string line in lines) {
+                        arrowCounts.Add(line.Split(',')[0], int.Parse(line.Split(',')[1]));
+                    }
+                } else {
+                    log("No stored data found for ArrowCounter. Initializing values to 0.");
+                    foreach (string arrowName in arrowNames) {
+                        arrowCounts.Add(arrowName, 0);
+                    }
+                }
+                return arrowCounts;
+            }
+
+            public static string getCountsFilePath() {
+                return Path.Combine(Paths.ConfigPath, countsFileName);
             }
         }
     }
