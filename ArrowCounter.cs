7a8bbeb036a581a65fbaea46f29780db7be3dd41
BruceOfTheBow <bruceofthebow@gmail.com>
2022-08-03 18:21:50 -0700

    ArrowCounter now tracks arrow count per player. Initializes from file based on pid on player load and saves on player save.

--- ArrowCounter.cs
+++ ArrowCounter.cs
@@ -1,13 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Reflection.Emit;
 using System.Linq;
 using BepInEx;
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using HarmonyLib;
-using UnityEngine;
 
 namespace ArrowCounter
 {
@@ -25,29 +23,12 @@ namespace ArrowCounter
         Harmony _harmony;
         static ConfigEntry<bool> isModEnabled;
 
+        private static bool _debug = true;
         public static bool ArrowCounterUIEnabled = true;
 
-        public void Awake()
-        {
-            isModEnabled = Config.Bind<bool>("_Global", "isModEnabled", true, "Enable or disable this mod.");
-            _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGuid);
-            _logger = Logger;
-            Attack_UseAmmo_Patch.initializeArrowCounts();
-        }
+        private static Dictionary<string, int> arrowCounts = new Dictionary<string, int>();
 
-        public void OnDestroy()
-        {
-            // this should really go on logout
-            Attack_UseAmmo_Patch.saveCounts();
-            _harmony?.UnpatchSelf();
-        }
-
-
-        [HarmonyPatch(typeof(Attack))]
-        public static class Attack_UseAmmo_Patch {
-            private static bool _debug = true;
-
-            private static List<string> arrowNames = new List<string> {
+        private static List<string> arrowNames = new List<string> {
                     "$item_arrow_wood",
                     "$item_arrow_fire",
                     "$item_arrow_flint",
@@ -59,69 +40,63 @@ namespace ArrowCounter
                     "$item_arrow_frost",
                     "$item_arrow_needle"
                 };
-            private static Dictionary<string, int> arrowCounts = initializeArrowCounts();            
 
-            [HarmonyPostfix]
-            [HarmonyPatch(nameof(Attack.UseAmmo))]
-            
-            public static void UseAmmoPostfix(Attack __instance)
-            {
-                string ammoType = Traverse.Create(__instance).Field("m_ammoItem").Field("m_shared").Field("m_ammoType").GetValue() as string;
-                string ammoName = Traverse.Create(__instance).Field("m_ammoItem").Field("m_shared").Field("m_name").GetValue() as string;
-                countAmmo(ammoType, ammoName);
-            }
+        public void Awake()
+        {
+            isModEnabled = Config.Bind<bool>("_Global", "isModEnabled", true, "Enable or disable this mod.");
+            _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGuid);
+            _logger = Logger;
+        }
 
-            private static bool countAmmo(string ammoType, string ammoName)
-            {
-                if(ammoType.Equals("$ammo_arrows"))
-                {
-                    arrowCounts[ammoName]++;
-                    log(arrowCounts[ammoName].ToString() + " " + ammoName.Split('_').Last() + " arrows fired in total.");
-                    return true;
-                }
-                return false;
-            }
+        public void OnDestroy()
+        {
+            _harmony?.UnpatchSelf();
+        }
 
-            private static void log(string message)
-            {
-                if(_debug)
-                {
-                    _logger.LogInfo(message);
-                }
-            }
-            
-            public static void GetCounts() {
-
-            }
-
-            public static void saveCounts() {
-                log("Arrow count saving to file: " + getCountsFilePath());
-                using (StreamWriter file = File.CreateText(getCountsFilePath())) {
+        public static void saveCounts(Player player) {
+            if(!player.GetPlayerName().IsNullOrWhiteSpace()) {
+                log("Arrow count saving for player: " + player.GetPlayerName());
+                using (StreamWriter file = File.CreateText(getCountsFilePath(player))) {
                     foreach (KeyValuePair<string, int> entry in arrowCounts) {
                         file.WriteLine(entry.Key + "," + entry.Value.ToString());
                     }
                 }
             }
-            public static Dictionary<string, int> initializeArrowCounts() {
-                Dictionary<string, int> arrowCounts = new Dictionary<string, int>();
-                if (File.Exists(getCountsFilePath())) {
-                    log("Reading data from " + getCountsFilePath());
-                    string[] lines = File.ReadAllLines(getCountsFilePath());
-                    foreach(string line in lines) {
+        }
+        public static string getCountsFilePath(Player player) {
+            return Path.Combine(Paths.ConfigPath, countsFileName + "_" + player.GetPlayerID().ToString());
+        }
+        public static void initializeArrowCounts(Player player) {
+            if(!player.GetPlayerName().IsNullOrWhiteSpace()) {
+                arrowCounts = new Dictionary<string, int>();
+                if (File.Exists(getCountsFilePath(player))) {
+                    log("Loading data for player " + player.GetPlayerName());
+                    string[] lines = File.ReadAllLines(getCountsFilePath(player));
+                    foreach (string line in lines) {
                         arrowCounts.Add(line.Split(',')[0], int.Parse(line.Split(',')[1]));
                     }
                 } else {
-                    log("No stored data found for ArrowCounter. Initializing values to 0.");
+                    log("No stored data found for player " + player.GetPlayerName() + " ArrowCounter. Initializing values to 0.");
                     foreach (string arrowName in arrowNames) {
                         arrowCounts.Add(arrowName, 0);
                     }
                 }
-                return arrowCounts;
+            } else {
+                log("No player name found.");
             }
-
-            public static string getCountsFilePath() {
-                return Path.Combine(Paths.ConfigPath, countsFileName);
+        }
+        public static void log(string message) {
+            if (_debug) {
+                _logger.LogInfo(message);
             }
+        }
+        public static bool countAmmo(string ammoType, string ammoName) {
+            if (ammoType.Equals("$ammo_arrows")) {
+                ArrowCounter.arrowCounts[ammoName]++;
+                ArrowCounter.log(ArrowCounter.arrowCounts[ammoName].ToString() + " " + ammoName.Split('_').Last() + " arrows fired in total.");
+                return true;
+            }
+            return false;
         }
     }
 }
---
+++ Patches.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace ArrowCounter {
+
+    [HarmonyPatch(typeof(Player), "Load")]
+    public static class PlayerLoadPatch {
+
+        [HarmonyPostfix]
+        public static void PlayerLoadPostfix(Player __instance) {
+            ArrowCounter.initializeArrowCounts(__instance);
+        }
+    }
+
+    [HarmonyPatch(typeof(Player), "Save")]
+    public static class PlayerOnDestroyPatch {
+
+        [HarmonyPrefix]
+        public static bool PlayerDestroyPrefix(Player __instance) {
+            ArrowCounter.saveCounts(__instance);
+            return true;
+        }
+        
+    }
+    [HarmonyPatch(typeof(Attack), "UseAmmo")]
+    public static class Attack_UseAmmo_Patch {
+        [HarmonyPostfix]
+        public static void UseAmmoPostfix(Attack __instance) {
+            //string ammoType = __instance.m_ammoItem.m_shared.m_ammoType;
+            //string ammoName = __instance.m_ammoItem.m_shared.m_name;
+            // Non Publicized way of accessing properties
+            string ammoType = Traverse.Create(__instance).Field("m_ammoItem").Field("m_shared").Field("m_ammoType").GetValue() as string;
+            string ammoName = Traverse.Create(__instance).Field("m_ammoItem").Field("m_shared").Field("m_name").GetValue() as string;
+            if (!string.IsNullOrWhiteSpace(ammoType) && !string.IsNullOrWhiteSpace(ammoName)) {
+                ArrowCounter.countAmmo(ammoType, ammoName);
+            }
+        }
+    }
+}
